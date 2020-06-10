08bff6b46f53dc0fcc7aa51e2c0fccac8ce408a0
Ali Bahaloo <alibahaloo@gmail.com>
2020-06-09 17:11:00 -0700

    fixes vpm-6

---
+++ Data/Queries/ReservationQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VPM.Data.Queries
+{
+    /*
+     * These are the items that Resevation lists can be quered against
+     * For ==Getting== reservation lists
+     */
+    public class ReservationQuery
+    {
+        public Guid BuildingId { get; set; } = Guid.Empty;
+        public string ApplicationUserId { get; set; }
+        public string Unit { get; set; }
+
+        [DisplayName("Plate Number")]
+        public string VehiclePlateNumber { get; set; }
+
+        [DisplayName("Show Expired Reservations")]
+        public bool ShowExpired { get; set; } = true;
+    }
+}
---
+++ Pages/Shared/Components/ReservationFilter/AvailableInputs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VPM.Pages.Shared.Components.ReservationFilter
+{
+    public class AvailableInputs
+    {
+        public bool ByValidity { get; set; }
+        public bool ByUnit { get; set; }
+        public bool ByVehiclePlateNumber { get; set; } = false;
+        public bool ByBuilding { get; set; }
+    }
+}
